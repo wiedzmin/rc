ecab6f690c48197b7da0d358ca9b031b430ea5bd
Alex <aaermolov@gmail.com>
2014-06-30 13:12:06 +0400

    generalized script for symlinks deployment

---
+++ make-symlinks.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CURRENT_DIR=`pwd`
+echo "Making symlinks to configs from ${CURRENT_DIR}..."
+
+ln -svf $CURRENT_DIR/.Xresources $HOME/.Xresources
+ln -svf $CURRENT_DIR/hgrc-linux $HOME/.hgrc
+ln -svf $CURRENT_DIR/.tmux.conf $HOME/.tmux.conf
+ln -svf $CURRENT_DIR/.xsession $HOME/.xsession
+ln -svf $CURRENT_DIR/zsh/.zsh $HOME/.zsh
+ln -svf $CURRENT_DIR/zsh/.zshrc $HOME/.zshrc
+
+sudo ln -svf $CURRENT_DIR/acpi /etc/acpi
+sudo ln -svf $CURRENT_DIR/xorg.conf.d /etc/X11
--- zsh/make-symlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CURRENT_DIR=`pwd`
-echo "Making symlinks to zsh config repository ($CURRENT_DIR --> $HOME)"
-
-ln -s $CURRENT_DIR/.zsh $HOME/.zsh
-ln -s $CURRENT_DIR/.zshrc $HOME/.zshrc
-
