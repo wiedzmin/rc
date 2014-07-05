00586b1594853aaa11d5e07895b7733ad0f3431a
Alex <aaermolov@gmail.com>
2014-07-06 01:04:43 +0400

    updated .vimperatorrc

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -10,6 +10,7 @@ ln -svf $CURRENT_DIR/.xsession $HOME/.xsession
 ln -svf $CURRENT_DIR/zsh/.zsh $HOME/.zsh
 ln -svf $CURRENT_DIR/zsh/.zshrc $HOME/.zshrc
 ln -svf $CURRENT_DIR/.xxkbrc $HOME/.xxkbrc
+ln -svf $CURRENT_DIR/.vimperatorrc $HOME/.vimperatorrc
 
 sudo ln -svf $CURRENT_DIR/acpi /etc/acpi
 sudo ln -svf $CURRENT_DIR/xorg.conf.d /etc/X11
