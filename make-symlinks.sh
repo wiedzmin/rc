42ea7be2e97b36b5e8e96a1bfe44d4e00ffe0405
Alex <aaermolov@gmail.com>
2014-07-06 18:19:26 +0400

    added emacsclient settings to enable org-protocol functionality

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -11,6 +11,11 @@ ln -svf $CURRENT_DIR/zsh/.zsh $HOME/.zsh
 ln -svf $CURRENT_DIR/zsh/.zshrc $HOME/.zshrc
 ln -svf $CURRENT_DIR/.xxkbrc $HOME/.xxkbrc
 ln -svf $CURRENT_DIR/.vimperatorrc $HOME/.vimperatorrc
+ln -svf $CURRENT_DIR/emacsclient.desktop $HOME/.local/share/applications
+cat >> $HOME/.local/share/applications/mimeapps.list <<EOF
+x-scheme-handler/org-protocol=emacsclient.desktop
+EOF
 
 sudo ln -svf $CURRENT_DIR/acpi /etc/acpi
 sudo ln -svf $CURRENT_DIR/xorg.conf.d /etc/X11
+sudo ln -svf $CURRENT_DIR/emacsclient.desktop /usr/share/applications
