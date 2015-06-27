67b2eb11e588e1c223048a037ce155ab765cbc1f
Alex Ermolov <aermolov@mirantis.com>
2015-06-27 12:55:29 +0300

    acpi: improved suspend/resume commands

--- acpi/lid.sh
+++ acpi/lid.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-sudo -u octocat emacsclient --eval '(jabber-disconnect)'
 
-if [[ -n `lsmod | grep e1000` ]]
-then
-	rmmod e1000e
-	rmmod e1000
+#!/bin/sh
+# check if the lid is open or closed, using the /proc file
+if grep closed /proc/acpi/button/lid/LID/state >/dev/null ; then
+    # if the lid is now closed, save the network state and suspend to RAM
+    sudo -u octocat emacsclient --eval '(jabber-disconnect)'
+    /usr/share/wicd/daemon/suspend.py
+    hibernate-ram
+else
+    # if the lid is now open, restore the network state.
+    # (if we are running, a wakeup has already occured!)
+    /usr/share/wicd/daemon/autoconnect.py
 fi
-hibernate-ram
 
 Sleep
