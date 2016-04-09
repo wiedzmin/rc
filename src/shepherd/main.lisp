305aa5037a60e079286bc457178bf8ad87405870
Alex Ermolov <aermolov@mirantis.com>
2016-04-09 03:51:46 +0300

    utils: introduced CL mail checking program
    Golang version seemed to be broken in some ways and go zombie after some
    time working.

---
+++ src/shepherd/main.lisp
@@ -0,0 +1,64 @@
+(in-package #:shepherd)
+
+(defmacro with-interactive-interrupt (&body body)
+  `(handler-case
+       (progn ,@body)
+     (sb-sys:interactive-interrupt ()
+       (format t "Exiting due to an interactive interrupt.~%")
+       (sb-ext:exit :code 1))))
+
+(defun get-now-timestamp ()
+  (multiple-value-bind
+        (second minute hour date month year day-of-week dst-p tz)
+      (get-decoded-time)
+    (format nil "[~2,'0d/~2,'0d/~d ~2,'0d:~2,'0d:~2,'0d (GMT~@d)]"
+            date
+            month
+            year
+            hour
+            minute
+            second
+            (- tz))))
+
+#+sbcl
+(defun check-mail-personal-full ()
+  (format t "~a Checking channel: aaermolov@gmail.com~%" (get-now-timestamp))
+  (sb-impl::run-program "/usr/bin/mbsync" '("aaermolov@gmail.com")))
+
+#+sbcl
+(defun check-mail-personal-inbox ()
+  (format t "~a Checking channel: aaermolov@gmail.com-inbox~%" (get-now-timestamp))
+  (sb-impl::run-program "/usr/bin/mbsync" '("aaermolov@gmail.com-inbox")))
+
+#+sbcl
+(defun check-mail-work-full ()
+  (format t "~a Checking channel: aermolov@mirantis.com~%" (get-now-timestamp))
+  (sb-impl::run-program "/usr/bin/mbsync" '("aermolov@mirantis.com")))
+
+#+sbcl
+(defun check-mail-work-inbox ()
+  (format t "~a Checking channel: aermolov@mirantis.com-inbox~%" (get-now-timestamp))
+  (sb-impl::run-program "/usr/bin/mbsync" '("aermolov@mirantis.com-inbox")))
+
+#+sbcl
+(defun update-notmuch-db ()
+  (format t "~a Updating notmuch DB~%" (get-now-timestamp))
+  (sb-impl::run-program "/usr/bin/notmuch" '("new")))
+
+#+sbcl
+(defun shepherd-main ()
+  (with-interactive-interrupt
+    (cl-cron:make-cron-job 'check-mail-personal-full :step-min 25)
+    (cl-cron:make-cron-job 'check-mail-personal-inbox :step-min 5)
+    (cl-cron:make-cron-job 'check-mail-work-full :step-min 15)
+    (cl-cron:make-cron-job 'check-mail-work-inbox :step-min 3)
+    (cl-cron:make-cron-job 'update-notmuch-db :step-min 2)
+    (cl-cron:start-cron)
+    (bordeaux-threads::join-thread cl-cron::*cron-dispatcher-thread*)))
+
+#+sbcl
+(defun save-image-shepherd ()
+  (sb-ext:save-lisp-and-die "shepherd"
+                            :compression t
+                            :executable t
+                            :toplevel #'shepherd-main))
---
+++ src/shepherd/package.lisp
@@ -0,0 +1,5 @@
+;;;; package.lisp
+
+(defpackage #:shepherd
+  (:use #:cl)
+  (:export #:shepherd-main))
