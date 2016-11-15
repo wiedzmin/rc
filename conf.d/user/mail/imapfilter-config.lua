4fa033db17e8a52cc07e31da4ccf5d269e6d2136
Alex Ermolov <aermolov@mirantis.com>
2016-11-15 23:19:55 +0300

    imapfilter: reordered config for better readability

--- conf.d/user/mail/imapfilter-config.lua
+++ conf.d/user/mail/imapfilter-config.lua
@@ -5,22 +5,10 @@ cmd_personal = io.popen('gpg2 -q --for-your-eyes-only --no-tty -d ~/docs/enc/cre
 out_personal = cmd_personal:read('*a')
 pass_personal = string.gsub(out_personal, '[\n\r]+', '')
 
-cmd_work = io.popen('gpg2 -q --for-your-eyes-only --no-tty -d ~/docs/enc/cred/aermolov@mirantis.com.gpg', 'r')
-out_work = cmd_work:read('*a')
-pass_work = string.gsub(out_work, '[\n\r]+', '')
-
-
 account_personal = IMAP {
  server = 'imap.gmail.com',
  username = 'aaermolov@gmail.com',
  password = pass_personal,
- ssl = 'ssl3'
- }
-
-account_work = IMAP {
- server = 'imap.gmail.com',
- username = 'aermolov@mirantis.com',
- password = pass_work,
  ssl = 'ssl3'
  }
 
@@ -29,6 +17,17 @@ account_personal['[Gmail]/Trash']:delete_messages(trash_personal)
 
 spam_personal = account_personal['[Gmail]/Spam']:is_unanswered()
 account_personal['[Gmail]/Spam']:delete_messages(spam_personal)
+
+cmd_work = io.popen('gpg2 -q --for-your-eyes-only --no-tty -d ~/docs/enc/cred/aermolov@mirantis.com.gpg', 'r')
+out_work = cmd_work:read('*a')
+pass_work = string.gsub(out_work, '[\n\r]+', '')
+
+account_work = IMAP {
+ server = 'imap.gmail.com',
+ username = 'aermolov@mirantis.com',
+ password = pass_work,
+ ssl = 'ssl3'
+ }
 
 trash_work = account_work['[Gmail]/Trash']:is_undeleted()
 account_work['[Gmail]/Trash']:delete_messages(trash_work)
