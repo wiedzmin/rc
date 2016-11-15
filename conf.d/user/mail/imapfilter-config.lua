95e0d4fdaa9012480fbfbe5574f574bfaa5abc39
Alex Ermolov <aermolov@mirantis.com>
2016-11-15 23:20:47 +0300

    imapfilter: removed obsoleted work config

--- conf.d/user/mail/imapfilter-config.lua
+++ conf.d/user/mail/imapfilter-config.lua
@@ -17,20 +17,3 @@ account_personal['[Gmail]/Trash']:delete_messages(trash_personal)
 
 spam_personal = account_personal['[Gmail]/Spam']:is_unanswered()
 account_personal['[Gmail]/Spam']:delete_messages(spam_personal)
-
-cmd_work = io.popen('gpg2 -q --for-your-eyes-only --no-tty -d ~/docs/enc/cred/aermolov@mirantis.com.gpg', 'r')
-out_work = cmd_work:read('*a')
-pass_work = string.gsub(out_work, '[\n\r]+', '')
-
-account_work = IMAP {
- server = 'imap.gmail.com',
- username = 'aermolov@mirantis.com',
- password = pass_work,
- ssl = 'ssl3'
- }
-
-trash_work = account_work['[Gmail]/Trash']:is_undeleted()
-account_work['[Gmail]/Trash']:delete_messages(trash_work)
-
-spam_work = account_work['[Gmail]/Spam']:is_unanswered()
-account_work['[Gmail]/Spam']:delete_messages(spam_work)
