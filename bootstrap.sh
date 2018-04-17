67e5311876e87b28ca14f4f091f64517eb05f736
Alex Ermolov <aaermolov@gmail.com>
2018-04-17 15:55:59 +0300

    centralized TODO entries

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,3 @@
 
 echo "=== This is boostrapping script to deploy personal configs. ==="
 echo "=== Nothing is here so far, come again later! ==="
-
-# TODO: https://github.com/jameskyle/ansible-gentoo
