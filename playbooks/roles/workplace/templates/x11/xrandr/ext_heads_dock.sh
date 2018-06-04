0302a999c2fff518f86b52d18b51ea6c6bd3d8c3
Alex Ermolov <aaermolov@gmail.com>
2018-06-04 19:07:40 +0300

    workplace: refactored ACPI setup

--- playbooks/roles/workplace/templates/x11/xrandr/ext_heads_dock.sh
+++ playbooks/roles/workplace/templates/x11/xrandr/ext_heads_dock.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
+export DISPLAY=:0
+export XAUTHORITY=/home/octocat/.Xauthority
 # multiple invocations because the Intel graphics card can only handle two outputs at a time
 xrandr -d :0.0 --output {{ xrandr_dock_left_output }} --auto --left-of {{ xrandr_internal_output }} --rotate {{ xrandr_dock_left_rotation}} --gamma 1:1:{{ xrandr_blue }} --brightness {{ xrandr_brightness }}
 xrandr -d :0.0 --output {{ xrandr_dock_right_output }} --auto --right-of {{ xrandr_internal_output }} --rotate {{ xrandr_dock_right_rotation }} --gamma 1:1:{{ xrandr_blue }} --brightness {{ xrandr_brightness }}
-
--- playbooks/roles/workplace/templates/x11/xrandr/ext_heads_undock.sh
+++ playbooks/roles/workplace/templates/x11/xrandr/ext_heads_undock.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export DISPLAY=:0
+export XAUTHORITY=/home/octocat/.Xauthority
 # multiple invocations because the Intel graphics card can only handle two outputs at a time
 xrandr -d :0.0 --output {{ xrandr_dock_left_output }} --off
 xrandr -d :0.0 --output {{ xrandr_dock_right_output }} --off
