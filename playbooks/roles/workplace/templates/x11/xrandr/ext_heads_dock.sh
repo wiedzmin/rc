5d6fb9f7f37e5532c6ccb8094ba22724acba799f
Alex Ermolov <aaermolov@gmail.com>
2018-06-04 20:46:22 +0300

    workplace: x11: execute custom commands for current WM after dock status changes

--- playbooks/roles/workplace/templates/x11/xrandr/ext_heads_dock.sh
+++ playbooks/roles/workplace/templates/x11/xrandr/ext_heads_dock.sh
@@ -5,3 +5,9 @@ export XAUTHORITY=/home/octocat/.Xauthority
 # multiple invocations because the Intel graphics card can only handle two outputs at a time
 xrandr -d :0.0 --output {{ xrandr_dock_left_output }} --auto --left-of {{ xrandr_internal_output }} --rotate {{ xrandr_dock_left_rotation}} --gamma 1:1:{{ xrandr_blue }} --brightness {{ xrandr_brightness }}
 xrandr -d :0.0 --output {{ xrandr_dock_right_output }} --auto --right-of {{ xrandr_internal_output }} --rotate {{ xrandr_dock_right_rotation }} --gamma 1:1:{{ xrandr_blue }} --brightness {{ xrandr_brightness }}
+
+{% if xsession_current_wm == "stumpwm" %}
+{% for command in x11_stumpwm_on_xrandr_commands %}
+/usr/bin/stumpish {{ command }}
+{% endfor %}
+{% endif %}
--- playbooks/roles/workplace/templates/x11/xrandr/ext_heads_undock.sh
+++ playbooks/roles/workplace/templates/x11/xrandr/ext_heads_undock.sh
@@ -6,3 +6,9 @@ export XAUTHORITY=/home/octocat/.Xauthority
 xrandr -d :0.0 --output {{ xrandr_dock_left_output }} --off
 xrandr -d :0.0 --output {{ xrandr_dock_right_output }} --off
 xrandr -d :0.0 --output {{ xrandr_internal_output }} --auto --primary # --gamma 1:1:{{ xrandr_blue }} --brightness {{ xrandr_brightness }}
+
+{% if xsession_current_wm == "stumpwm" %}
+{% for command in x11_stumpwm_on_xrandr_commands %}
+/usr/bin/stumpish {{ command }}
+{% endfor %}
+{% endif %}
