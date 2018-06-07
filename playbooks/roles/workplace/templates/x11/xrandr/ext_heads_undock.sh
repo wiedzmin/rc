c00405cdd59e371ee91cc86d82221e74bbe14253
Alex Ermolov <aaermolov@gmail.com>
2018-06-07 19:23:51 +0300

    workplace: x11: execute arbitrary commands for current WM on XRandR status changes

--- playbooks/roles/workplace/templates/x11/xrandr/ext_heads_undock.sh
+++ playbooks/roles/workplace/templates/x11/xrandr/ext_heads_undock.sh
@@ -8,7 +8,23 @@ xrandr -d :0.0 --output {{ xrandr_dock_right_output }} --off
 xrandr -d :0.0 --output {{ xrandr_internal_output }} --auto --primary # --gamma 1:1:{{ xrandr_blue }} --brightness {{ xrandr_brightness }}
 
 {% if xsession_current_wm == "stumpwm" %}
+{% if x11_stumpwm_on_xrandr_commands is defined %}
 {% for command in x11_stumpwm_on_xrandr_commands %}
 /usr/bin/stumpish {{ command }}
 {% endfor %}
+{% endif %}
+{% endif %}
+
+{% if xsession_current_wm == "xmonad" %}
+{% if x11_xmonad_on_xrandr_commands is defined %}
+# Under construction
+{% endif %}
+{% endif %}
+
+{% if xsession_current_wm == "awesome" %}
+{% if x11_awesomewm_on_xrandr_commands is defined %}
+{% for command_line in x11_awesomewm_on_xrandr_commands %}
+echo '{{ command_line }}' | awesome-client
+{% endfor %}
+{% endif %}
 {% endif %}
